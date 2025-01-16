1639a5eccb2d2d7e67970a4f48cd2c56daa2ef4e
leksetengah <st.pietra@gmail.com>
2025-01-16 15:51:20 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="fon-bold text-blue-500">Welcome to the course!</h1>
+    </div>
+  );
+}
