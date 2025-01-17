b514659ecc2378ccea1e2fcb377e15c2ef9d03cc
leksetengah <st.pietra@gmail.com>
2025-01-17 15:52:15 +0700

    basic setup, shadcn ui, theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="fon-bold text-blue-500">Welcome to the course!</h1>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle />
+      </div>
+      <h1 className="fon-bold">Welcome to the course!</h1>
+      <h1 className="fon-bold text-blue-500 font-barlow">Welcome to the course!</h1>
+      <Button variant={"destructive"}>Click Here</Button>
     </div>
   );
 }
